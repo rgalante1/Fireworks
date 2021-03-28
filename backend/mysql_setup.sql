d8814daf4aeab5cf3405fb4d8a60f31c81e78515
Riley Galante <rgalante@rileys-mbp-2.attlocal.net>
2021-03-28 17:13:33 -0500

    database building

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -5,14 +5,29 @@ CREATE DATABASE db;
 USE db;
 
 -- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+CREATE TABLE `db`.`user` (
+    `userID` INT NOT NULL AUTO_INCREMENT, 
+    `username` VARCHAR(45), 
+    `password` VARCHAR(45),
+    `firstName` VARCHAR(45),
+    `lastName` VARCHAR(45),
+    `phone` VARCHAR(45),
+    `userType` VARCHAR(45),
+    `mail` VARCHAR(45),
+    `employerID` VARCHAR(45),
+    PRIMARY KEY (`userID`), 
+    -- UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
+
+CREATE TABLE `db`.`post` (
+    `companyID` INT(10),
+    `title` VARCHAR(45),
+    `description` VARCHAR(45),
+    FOREIGN KEY (`companyID`)
+        REFERENCES company(`companyID`)
 );
 
 -- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+-- INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
 
 
