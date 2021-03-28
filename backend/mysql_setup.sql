29ebd94be95e7f22d5ff278c4dbc6bede81b1248
Naishur-M <naishurmal@gmail.com>
2021-03-28 17:15:56 -0500

    sql file table creation

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -5,11 +5,41 @@ CREATE DATABASE db;
 USE db;
 
 -- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+CREATE TABLE `db`.`invites` (
+    `userID` INT(10), 
+	`dateSent` DATE, 
+	`accepted` INT(1),
+	`meetingID` INT(10),
+	`senderID` INT(10),
+	`inviteType` INT(1),
+	
+    FOREIGN KEY (`userID`)
+		REFERENCES user(`userID`),
+		
+	FOREIGN KEY (`meetingID`), 
+		REFERENCES metting(`meetingID`)
+    
+);
+
+-- create table in db
+CREATE TABLE `db`.`meeting` (
+	`meetingID` INT(10) NOT NULL AUTO_INCREMENT,
+	`description` VARCHAR(25), 
+	`startTime` Date, 
+	`endTime` Date,
+	`meetingLink` VARCHAR(25), 
+	`hostCompanyID` VARCHAR(25), 
+	`location` VARCHAR(25), 
+	`meetingType` VARCHAR(25),
+	`rating` INT(10), 
+	`attendees` INT(10), 
+	`eventDate` Date, 
+	
+	PRIMARY KEY (`meetingID`)
+		
+	FOREIGN KEY (`hostCompanyID`), 
+		REFERENCES company(`hostCompanyID`)
+		
 );
 
 -- insert sample entry
