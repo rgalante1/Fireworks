6ee74f0ee9bb203e95fd8c58e88e0b64630b3c88
alexjmeech <alexjmeech@gmail.com>
2021-04-14 03:21:10 -0500

    Add Post display component and a frontend route to view posts by id

---
+++ frontend/src/models/Post.js
@@ -0,0 +1,12 @@
+class Post {
+    constructor(id, title, description, date, location, meetingLink) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.meetingLink = meetingLink;
+    }
+}
+
+export default Post;
